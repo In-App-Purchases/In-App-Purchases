1d6d6263457e88e3620fb24a67eebf2344dfdc58
Noah Katz <noahkatz@gmail.com>
2021-12-13 10:59:14 -0500

    moved firebaseConfi

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, child, onValue, update } from "firebase/database";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-const firebaseConfig = {
-  apiKey: "AIzaSyDncQ5Oi670XWPju9h12GXZO7mmkIRcnLk",
-  authDomain: "in-app-purchases-474.firebaseapp.com",
-  databaseURL: "https://in-app-purchases-474-default-rtdb.firebaseio.com",
-  projectId: "in-app-purchases-474",
-  storageBucket: "in-app-purchases-474.appspot.com",
-  messagingSenderId: "854630314130",
-  appId: "1:854630314130:web:2c9def1db93adedd12364b",
-  measurementId: "G-3TLCX4C6X4"
-};
+import {firebaseConfig} from "./firebaseConfig.js";
+
 const fbapp = initializeApp(firebaseConfig);
 const db = getDatabase(fbapp);
 const auth = getAuth(fbapp);
