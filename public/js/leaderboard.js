de530d3cb41da666c797f33ee9154aa77ad0efa4
Noah Katz <noahkatz@gmail.com>
2021-12-13 09:48:23 -0500

    leaderboard color changes

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -24,10 +24,10 @@ const renderUsers = (data) => {
     $('#leaderStats').append(`
     <hr />
     <div class="row">
-      <div class="col-1"></div>
+      <div class="col-2"></div>
       <div class="col-2"><strong><p class="lg-text">${parseInt(index)+1}</p></strong></div>
-      <div class="col-5"><strong><p class="lg-text"><a href="mailto:${data[index].email}" style="color: black; text-decoration: underline">${data[index].email}</a></p></strong></div>
-      <div class="col-3"><strong><p class="lg-text">${data[index].count}</p></strong></div>
+      <div class="col-4"><strong><p class="lg-text"><a href="mailto:${data[index].email}" style="color: aqua; text-decoration: underline">${data[index].email}</a></p></strong></div>
+      <div class="col-3"><strong><p class="lg-text" style="text-align: left">${data[index].count}</p></strong></div>
       <div class="col-1"></div>
     </div>
     <hr />
