d3057b3d104c8d9557f08ac206faaa8a530625f1
Noah Katz <noahkatz@gmail.com>
2021-12-14 01:15:24 -0500

    fixed leaderboard

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -27,7 +27,7 @@ const renderUsers = (data) => {
       <div class="col-2"></div>
       <div class="col-2"><p class="lg-text">${parseInt(index)+1}</p></div>
       <div class="col-4"><p class="lg-text"><a href="mailto:${data[index].email}" style="text-decoration: underline">${data[index].email}</a></p></div>
-      <div class="col-3"><p class="lg-text" style="text-align: left">${data[index].count}</p></div>
+      <div class="col-3"><p class="lg-text" style="text-align: right">${data[index].count}</p></div>
       <div class="col-1"></div>
     </div>
     <hr />
