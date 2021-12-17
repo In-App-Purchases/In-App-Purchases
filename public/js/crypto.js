8926d852bf5d2c34d96dbd90cf2f44a8d1e4476b
Jake Screen <jakescreen@Jakes-MacBook-Pro.local>
2021-12-17 14:18:00 -0500

    jake screen comments for crypto.js

--- public/js/crypto.js
+++ public/js/crypto.js
@@ -1,22 +1,22 @@
 let a = 0;
 
-let costs = [10,100,200,400,1000,100,200,400,800,1200,1000,2000,2000,4000,5000];
-let origCosts = [10,100,200,400,1000,100,200,400,800,1200];
-let speedUp = [2,10,50,100,200,1,2,5,7,10];
-let origSpeedUp = [2,10,50,100,200,1,2,5,7,10];
+let costs = [10,100,200,400,1000,100,200,400,800,1200,1000,2000,2000,4000,5000]; //costs of each upgrade
+let origCosts = [10,100,200,400,1000,100,200,400,800,1200,1000,2000,2000,4000,5000];// base costs of upgrades for use when resetting
+let speedUp = [2,10,50,100,200,1,2,5,7,10];// base speed of each upgrade, effects how much currency gained with each upgrade
+let origSpeedUp = [2,10,50,100,200,1,2,5,7,10]; // base speeds for use when resetting
 let count = +($("#counter").text()); // initial count value
 let delta = 1; // initial change in count per click
-let rate = 0;
-let randomUp = 0;
-var eventOn = false;
-var eventCountdown = 0;
-let prestige = 1;
-let saveData = {manual: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, auto: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, event: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, count: 0, delta: 0};
-let origSave = {manual: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, auto: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, event: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, money: count};
-var eventID = 0;
-let events = {};
-let spawnRate = 30;
-let userCoin;
+let rate = 0; // currency gain per second
+let randomUp = 0; // failsafe for random events, triggers a random event once over 500
+var eventOn = false; // whether an event is going on currently
+var eventCountdown = 0; // # of seconds left in current event
+let prestige = 1; // how many times the user has prestiged
+let saveData = {manual: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, auto: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, event: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, count: 0, delta: 0}; // keeps track of how many upragdes the user has purchased
+let origSave = {manual: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, auto: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, event: {up1: 0,up2: 0,up3: 0,up4: 0,up5: 0}, money: count}; // # of upgrades user has purchased, used when resetting
+var eventID = 0; // which event is currently happening
+let events = {}; //unused
+let spawnRate = 30; // rate at which currency appears in screen to click
+let userCoin; // which coin icon the user has chosen
 //a
 let sketch = (p) => {
     
@@ -176,6 +176,7 @@ jQuery(() => {
 
     $("#coinImg").on('click', click);
 
+    // queries server to prestige the user, resetting upgrades and increases currency per second by new prestige multiplier
     $("#prestigeButton").on('click', function(){
       $.post('/prestige', (res) => {
         if(res=='success') window.location.href = '/';
@@ -193,65 +194,68 @@ jQuery(() => {
       });
     });
 
+    // called when user clicks one of the upgrades in the manual tab
     const purchaseUpgrade = (upNum) => {
-      var upSave = "up" + (upNum  + 1).toString();
-        if(saveData.manual[upSave] >= 10){
-          document.getElementById('upgrade' + (upNum + 1).toString()).hidden = true;
+      var upSave = "up" + (upNum  + 1).toString(); // locates which upgrade to update in the saveData array
+        if(saveData.manual[upSave] >= 10){ //check for # of upgrades purchased is at max amount
+          document.getElementById('upgrade' + (upNum + 1).toString()).hidden = true; //hides upgrade button if at maximum
         }
-        if(count >= costs[upNum]){
-            count = count - costs[upNum];
-            delta = delta + speedUp[upNum];
-            costs[upNum] = Math.ceil(costs[upNum] * 1.5);
-            $("#banner").text('');
+        if(count >= costs[upNum]){ // check if user has enough currency to buy upgrade
+            count = count - costs[upNum]; // removes used currency
+            delta = delta + speedUp[upNum]; // increases currency gain per click based on upgrade purchased
+            costs[upNum] = Math.ceil(costs[upNum] * 1.5); // increase cost of next purchase
+            $("#banner").text(''); // makes sure 'insufficient currency icon does not show up
         }
         else{
-            $("#banner").text('Insufficient Currency');
-            setTimeout(() => {$('#banner').text('');}, 2000);
+            $("#banner").text('Insufficient Currency'); // tells user they do not have enough money
+            setTimeout(() => {$('#banner').text('');}, 2000); // fades away banner after 2 seconds
         }
-        $("#counter").text(`coins: ${Math.floor(count)}`);
-        var upLoc = "#costSpeed" + (upNum+1);
-        var upSave = "up" + (upNum + 1).toString();
-        saveData.manual[upSave] = saveData.manual[upSave] + 1;
-        $(upLoc).text(costs[upNum]);
+        $("#counter").text(`coins: ${Math.floor(count)}`); // updates how much currency user has
+        var upLoc = "#costSpeed" + (upNum+1); // locate button of upgrade
+        var upSave = "up" + (upNum + 1).toString(); // locates save location for upgrade
+        saveData.manual[upSave] = saveData.manual[upSave] + 1; // update save data with new number of purchased upgrades
+        $(upLoc).text(costs[upNum]); // updates new cost of next upgrade
     }
 
+    //called when user buys upgrade from automatic tab
     const purchaseSpeed = (upNum) => {
-        var upSave = "up" + (upNum  - 4).toString();
-        if(saveData.auto[upSave] >= 10){
-          document.getElementById('speed' + (upNum  - 4).toString()).hidden = true;
+        var upSave = "up" + (upNum  - 4).toString(); // locates which upgrade is being purchased
+        if(saveData.auto[upSave] >= 10){ // checks if user has purchased maximum amount of upgrades for this one
+          document.getElementById('speed' + (upNum  - 4).toString()).hidden = true; // hides upgrade if max # has been purchased
         }
-        if(count >= costs[upNum]){
-            count = count - costs[upNum];
-            rate = rate + speedUp[upNum];
-            costs[upNum] = Math.ceil(costs[upNum] * 1.5);
-            $("#banner").text('');
+        if(count >= costs[upNum]){ // checks if user has enough currency to purchase upgrade
+            count = count - costs[upNum]; // takes away currency used foer upgrade
+            rate = rate + speedUp[upNum]; // increases rate of currency gain per second based on upgrade purchased
+            costs[upNum] = Math.ceil(costs[upNum] * 1.5); // increases cost of next upgrade purchased
+            $("#banner").text(''); // resets banner of any messages
         }
         else{
-            $("#banner").text('Insufficient Currency');
-            setTimeout(() => {$('#banner').text('');}, 2000);
+            $("#banner").text('Insufficient Currency'); // tells user they do not posses enough currency
+            setTimeout(() => {$('#banner').text('');}, 2000); // resets 'insufficient currency message 
         }
-        $("#counter").text(`coins: ${Math.floor(count)}`);
-        var upLoc = "#costSpeed" + (upNum+1);
+        $("#counter").text(`coins: ${Math.floor(count)}`); // updates display with ammount of currency user has
+        var upLoc = "#costSpeed" + (upNum+1); // which button was being pressed
         
-        saveData.auto[upSave] = saveData.auto[upSave] + 1;
-        $(upLoc).text(costs[upNum]);
+        saveData.auto[upSave] = saveData.auto[upSave] + 1; // update save data with amount of upgrades user has purchased
+        $(upLoc).text(costs[upNum]);// updates cost on button
     }
 
     const purchaseEvent = (upNum) => {
-      var upSave = `up${upNum-10}`;
-      if(count >= costs[upNum]) {
-        count = count - costs[upNum];
-        doEvent(upNum);
-        $('#banner').text('');
-        document.getElementById('event' + (upNum  - 9).toString()).hidden = true;
-        saveData.auto[upSave] = saveData.auto[upSave] + 1;
+      var upSave = `up${upNum-10}`; // gets location of which button is pressed
+      if(count >= costs[upNum]) {//checks if user has enough currency
+        count = count - costs[upNum]; // removes currency used to purchase upgrade
+        doEvent(upNum); // calls doEvent function, which doubles spawn rate of coins
+        $('#banner').text(''); // clears banner of any messages
+        document.getElementById('event' + (upNum  - 9).toString()).hidden = true; // hides button of purchased upgrade
+        saveData.auto[upSave] = saveData.auto[upSave] + 1; // updates save data with number of grades purchased
       } else {
-        $('#banner').text('Insufficient Currency');
-        setTimeout(() => {$('#banner').text('');}, 2000);
+        $('#banner').text('Insufficient Currency'); // tells user they do not have enough money
+        setTimeout(() => {$('#banner').text('');}, 2000); // removes message after 2 seconds
       }
-      $("#counter").text(`coins: ${Math.floor(count)}`);
+      $("#counter").text(`coins: ${Math.floor(count)}`); // updates currency possesed by user
     };
 
+    //doubles spawn rate of coins on screen
     const doEvent = (val) => {
       spawnRate*=2;
     };
@@ -265,9 +269,10 @@ jQuery(() => {
         $.post(`/saveEvent/${saveData.event.up1}/${saveData.event.up2}/${saveData.event.up3}/${saveData.event.up4}/${saveData.event.up5}`);
     }, 8000);
 
+    //in-game clock, updates every second, does random events, updates currency gains, check if can prestige
     setInterval(function(){
-      var can_prestige = false;
-        for(var i = 1; i < 6; i++){
+      var can_prestige = false; // boolean on whether the user can prestige or not
+        for(var i = 1; i < 6; i++){ // checks if user has purchased all upgrades from manual tab
           var upSave = "up" + i.toString();
           if(saveData.manual[upSave] >= 10){
             can_prestige = true;
@@ -276,7 +281,7 @@ jQuery(() => {
             can_prestige = false;
           }
         }
-        for(var i = 1; i < 6; i++){
+        for(var i = 1; i < 6; i++){ // checks if user has purchased all upgrades from automatic tab
           var upSave = "up" + i.toString();
           if(saveData.auto[upSave] >= 10){
             can_prestige = true;
@@ -286,54 +291,54 @@ jQuery(() => {
           }
         }
         if(can_prestige == true){
-          setTimeout(()=>{document.getElementById("prestigeButton").hidden = false;},8000);
+          setTimeout(()=>{document.getElementById("prestigeButton").hidden = false;},8000); // shows prestige button if all upgrades purchased
         }
         else{
-          document.getElementById("prestigeButton").hidden = true;
+          document.getElementById("prestigeButton").hidden = true; // hides prestige button if not all upgrades purchased
         }
-        var chance =  Math.floor(Math.random() * 500);
-        if(eventOn == true){
-            $("#eventSign").text(`Event is Happening! Time Remaining: ${eventCountdown}`);
-            eventCountdown--;
-            if(eventCountdown == 0){
+        var chance =  Math.floor(Math.random() * 500); // chance for random event to occur
+        if(eventOn == true){ // is there an event currenctly occuring
+            $("#eventSign").text(`Event is Happening! Time Remaining: ${eventCountdown}`); // unused
+            eventCountdown--; // decreases remaining time for current event
+            if(eventCountdown == 0){ // if event is over
                 eventOn = false;
                 eventID = 0;
             }
-            switch(eventID){
+            switch(eventID){ // which event is going on
                 case 0:
-                    delta *= 2;
-                    rate *= 2;
+                    delta *= 2; // doubles coin increase pre click
+                    rate *= 2; // oubles coin gain per second
                     console.log("influencer");
-                    $("#eventSign").text(`Influencer event! (Growth Rate x2)`);
+                    $("#eventSign").text(`Influencer event! (Growth Rate x2)`); // displays which event is occuring currently
                     break;
                 case 1:
-                    count = 0;
+                    count = 0; // resets user currency
                     console.log("crash");
-                    $("#eventSign").text(`Market crash! (Money? Never heard of it.)`);
+                    $("#eventSign").text(`Market crash! (Money? Never heard of it.)`); // displays current event
                     break;
             }
 
-            $("#counter").text(`coins: ${Math.floor(count)}`);
+            $("#counter").text(`coins: ${Math.floor(count)}`); // update currency count of user
         }
         else{
-            $("#eventSign").text('No Current Events');
-            if(chance == 9 || randomUp >= 1000){
-                randomUp = 0;
-                eventID =  Math.floor(Math.random() * 2);
+            $("#eventSign").text('No Current Events'); // tells user no event is currently happening
+            if(chance == 9 || randomUp >= 1000){ // trigger a random event based on chance variable or number of clicks
+                randomUp = 0; // reset failsafe click count
+                eventID =  Math.floor(Math.random() * 2); // selects a random event
                 console.log(eventID);
-                eventOn = true;
-                eventCountdown = 10;
+                eventOn = true; // sets reandom event to be active
+                eventCountdown = 10; // sets timer for random event
                // console.log("event happens");
             }
             else{
                 //console.log("no event");
             }
-            count = count + (rate * prestige);
+            count = count + (rate * prestige); // updates user currency based on per second rate and prestige level
             $("#counter").text(`coins: ${Math.floor(count)}`);
         }
 
     }, 1000);
-
+    // this whole section sets the on click functionality for all upgrades, playing their sounds and the respective upgrade functions
     $("#upgrade1").click(function(){
         playAudio2('pick_upgrade');
        purchaseUpgrade(0);
@@ -464,7 +469,7 @@ const loadEvent = () => {
 function playAudio(url){ //deprecated
     new Audio(url).play();
 }
-
+// plays audio based on supplied link to sound effect
 function playAudio2(id){
     var x = document.getElementById(id);
     x.play();
