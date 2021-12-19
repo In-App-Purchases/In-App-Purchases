6249770e7127db1e2c1dcc80a3aeb2326fc362fb
jt1018 <48070252+jt1018@users.noreply.github.com>
2021-12-19 16:03:02 -0500

    comments/fixes on css, comments on modes.js, removed crypto.css

--- public/js/modes.js
+++ public/js/modes.js
@@ -1,10 +1,10 @@
 const btn = document.querySelector(".btn-toggle");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function () { /*when the dark mode button is clicked it toggles the dark theme is toggled on*/
   document.body.classList.toggle("dark-theme");
 });
 
-function buttontext() {
+function buttontext() { /*this changes the text on the button that changes the color theme depending on if dark mode is active*/
   if ($('body').hasClass('dark-theme')) {
     document.getElementById("themeswitch").innerHTML="Dark Mode?";
   }
